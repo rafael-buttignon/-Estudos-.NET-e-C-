facb6f5ef880b6dea39698b556d69f0d1ecc5608
Rafael Buttignon <rafaaugustocontato@gmail.com>
2020-07-21 17:17:24 -0300

    ByteBank Create New Account

---
+++ 2-ByteBank POO/ByteBank/01-ByteBank/Program.cs
@@ -0,0 +1,31 @@
+﻿  using System;
+
+namespace _01_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente contaDaGabi = new ContaCorrente();
+
+            contaDaGabi.titular = "Gabriela";
+            contaDaGabi.agencia = 863;
+            contaDaGabi.numero = 876644;
+            contaDaGabi.saldo = 100;
+
+
+            Console.WriteLine(contaDaGabi.titular);
+            Console.WriteLine("Agência: " + contaDaGabi.agencia);
+            Console.WriteLine("Número: " + contaDaGabi.numero);
+            Console.WriteLine("Saldo: " + contaDaGabi.saldo);
+            Console.WriteLine();
+
+            contaDaGabi.saldo += 200;
+            Console.WriteLine("Novo Saldo " + contaDaGabi.saldo);
+
+
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+}
