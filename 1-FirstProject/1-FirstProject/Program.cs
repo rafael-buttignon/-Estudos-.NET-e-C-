1513d42b1f62df9ec4d6b6aba0b6560f619fb955
Rafael Buttignon <rafaaugustocontato@gmail.com>
2020-07-20 22:05:50 -0300

    First Class Alura

---
+++ 1-FirstProject/1-FirstProject/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Dynamic;
+
+namespace _1_FirstProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá mundo. Review para certificação C# [Alura] ");
+
+
+            Console.WriteLine("A execução acabou. Tecle enter para finalizar . . . ");
+            Console.ReadLine();
+        }
+    }
+}
