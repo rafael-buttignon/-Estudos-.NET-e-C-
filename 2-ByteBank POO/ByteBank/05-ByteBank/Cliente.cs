30f1029c3cc2490b749220c8868b2018f1b420b2
Rafael Buttignon <rafaaugustocontato@gmail.com>
2020-07-22 16:48:09 -0300

    Relation with others class

---
+++ 2-ByteBank POO/ByteBank/05-ByteBank/Cliente.cs
@@ -0,0 +1,9 @@
+﻿namespace _05_ByteBank
+{
+    public class Cliente
+    {
+        public string nome;
+        public string cpf;
+        public string profissao;
+    }
+}
---
+++ 2-ByteBank POO/ByteBank/05-ByteBank/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _05_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cliente gabriela = new Cliente();
+
+            gabriela.nome = "Gabriela";
+            gabriela.profissao = "Programadora";
+            gabriela.cpf = "24454545466";
+
+            ContaCorrente conta = new ContaCorrente();
+            conta.titular = gabriela;
+            conta.agencia = 563;
+            conta.saldo = 500;
+            conta.numero = 5664234;
+
+            conta.titular.nome = "Gabriela Costa";
+
+            Console.WriteLine(gabriela.nome);
+            Console.WriteLine(conta.titular.nome);
+
+            Console.ReadLine();
+        }
+    }
+}
