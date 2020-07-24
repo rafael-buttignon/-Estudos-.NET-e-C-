7792d88a9a99ca38aaf22b5fda0377a2813e0a3f
Rafael Buttignon <rafaaugustocontato@gmail.com>
2020-07-23 22:29:11 -0300

    ByteBank herança e Interface

---
+++ 3-ByteBank Herança e Interface/ByteBank/ByteBank/Funcionarios/Funcionario.cs
@@ -0,0 +1,25 @@
+﻿namespace ByteBank.Funcionarios
+{
+    public class Funcionario
+    {
+        private int _tipo;
+        public string Nome { get; set; }
+        public string CPF { get; set; }
+        public double Salario { get; set; }
+
+        public Funcionario(int tipo)
+        {
+            _tipo = tipo;
+        }
+
+        public double GetBonificacao()
+        {
+            if(_tipo == 1)
+            {
+                return Salario;
+            }
+
+            return Salario * 0.10;
+        }
+    }
+}
---
+++ 3-ByteBank Herança e Interface/ByteBank/ByteBank/Program.cs
@@ -0,0 +1,22 @@
+﻿using ByteBank.Funcionarios;
+using System;
+
+namespace ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario carlos = new Funcionario(1);
+
+            carlos.Nome = "Carlos";
+            carlos.CPF = "784.224.664.20";
+            carlos.Salario = 2000;
+
+
+            Console.WriteLine(carlos.Nome); ;
+            Console.WriteLine(carlos.GetBonificacao());
+            Console.ReadLine();
+        }
+    }
+}
