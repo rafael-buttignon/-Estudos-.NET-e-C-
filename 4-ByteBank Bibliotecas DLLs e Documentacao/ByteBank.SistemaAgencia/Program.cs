934a58b9448f5276b198ec7584d8d604538a0fc0
Rafael Buttignon <rafaaugustocontato@gmail.com>
2020-07-30 18:10:32 -0300

    Bibliotecas de classes .DLL

---
+++ 4-ByteBank Bibliotecas DLLs e Documentacao/ByteBank.SistemaAgencia/Program.cs
@@ -0,0 +1,16 @@
+﻿using ByteBank.Modelos;
+using System;
+
+namespace ByteBank.SistemaAgencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente(854, 3423443);
+
+            Console.WriteLine(conta.Numero);
+            Console.ReadLine();
+        }
+    }
+}
