d00ecc4d4f419f7c0dfc589484f0932b2ca60c21
Rafael Buttignon <rafaaugustocontato@gmail.com>
2020-08-03 20:50:12 -0300

    Nuget, Install-Package

--- 4-ByteBank Bibliotecas DLLs e Documentacao/ByteBank.SistemaAgencia/Program.cs
+++ 4-ByteBank Bibliotecas DLLs e Documentacao/ByteBank.SistemaAgencia/Program.cs
@@ -1,5 +1,6 @@
 ﻿using ByteBank.Modelos;
 using ByteBank.Modelos.Funcionarios;
+using Humanizer;
 using System;
 
 namespace ByteBank.SistemaAgencia
@@ -8,11 +9,30 @@ namespace ByteBank.SistemaAgencia
     {
         static void Main(string[] args)
         {
-            ContaCorrente conta = new ContaCorrente(854, 3423443);
-            Funcionario funcionario = null;
+            //ContaCorrente conta = new ContaCorrente(854, 3423443);
+            //Funcionario funcionario = null;
 
-            Console.WriteLine(conta.Numero);
+            //Console.WriteLine(conta.Numero);
+
+            DateTime dataFimPagamento = new DateTime(2020, 12, 06);
+            DateTime dataCorrente = DateTime.Now;
+
+            TimeSpan diferenca = dataFimPagamento - dataCorrente;
+            TimeSpan diferencateste = TimeSpan.FromMinutes(60);
+
+            string mensagem = "Vencimento em " + TimeSpanHumanizeExtensions.Humanize(diferenca);
+            string mensagem1 = "Vencimento em " + TimeSpanHumanizeExtensions.Humanize(diferencateste);
+
+
+            Console.WriteLine(mensagem);
+            Console.WriteLine(mensagem1);
+
+
+            Console.WriteLine(dataCorrente);
+            Console.WriteLine(dataFimPagamento);
+
             Console.ReadLine();
         }
+
     }
 }
