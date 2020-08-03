be564ef8ea8572726edd04409ce973ad2fb91b75
Rafael Buttignon <rafaaugustocontato@gmail.com>
2020-08-03 20:03:56 -0300

    Documentando projeto exemple

--- 4-ByteBank Bibliotecas DLLs e Documentacao/ByteBank.Modelos/ContaCorrente.cs
+++ 4-ByteBank Bibliotecas DLLs e Documentacao/ByteBank.Modelos/ContaCorrente.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 
 namespace ByteBank.Modelos
 {
+    /// <summary>
+    /// Define uma conta Corrente do banco ByteBank
+    /// </summary>
     public class ContaCorrente
     {
         private static int TaxaOperacao;
@@ -38,6 +41,11 @@ namespace ByteBank.Modelos
             }
         }
 
+        /// <summary>
+        /// Cria uma instância de ContaCorrente com os argumentos utilizados
+        /// </summary>
+        /// <param name="agencia">Representa o valor da propriedade <see cref="Agencia"/> e deve possuir um valor maior que zero. </param>
+        /// <param name="numero">Representa o valor da propriedade <see cref="Numero"/> e deve possuir um valor maior que zero.</param>
         public ContaCorrente(int agencia, int numero)
         {
             if (numero <= 0)
@@ -56,7 +64,12 @@ namespace ByteBank.Modelos
             TotalDeContasCriadas++;
             TaxaOperacao = 30 / TotalDeContasCriadas;
         }
-
+        /// <summary>
+        /// Realiza o saque e a atualiza o valor da propriedade <see cref="Saldo"/>
+        /// </summary>
+        /// <exception cref="ArgumentException"> Exceção lançada quando um valor negativo e utilizado no argumento <paramref name="valor"/></exception>
+        /// <exception cref="SaldoInsuficienteException"> Exceção lançada quando o valor de <paramref name="valor"/> é maior que o valor da propriedade <see cref="Saldo"/>.</exception>
+        /// <param name="valor">Representa o valor do saque, deve ser maior que 0 e menor que o <see cref="Saldo"/></param>
         public void Sacar(double valor)
         {
             if (valor < 0)
