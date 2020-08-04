a8dc87ac549dc875028da6e64c718b2028a0922d
Rafael Buttignon <rafaaugustocontato@gmail.com>
2020-08-04 20:28:15 -0300

    Exemple Substring

---
+++ 5-ByteBank Strings, expressões regulares e Objects/ByteBank.Modelos/AutenticacaoHelper.cs
@@ -0,0 +1,10 @@
+﻿namespace ByteBank.Modelos
+{
+    internal class AutenticacaoHelper
+    {
+        public bool CompararSenhas(string senhaVerdadeira, string senhaTentativa)
+        {
+            return senhaVerdadeira == senhaTentativa;
+        }
+    }
+}
---
+++ 5-ByteBank Strings, expressões regulares e Objects/ByteBank.SistemaAgencia/Program.cs
@@ -0,0 +1,31 @@
+﻿using ByteBank.Modelos;
+using ByteBank.Modelos.Funcionarios;
+using Humanizer;
+using System;
+
+namespace ByteBank.SistemaAgencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string url = "pagina?argumentos";
+
+            //Console.WriteLine(url);
+            //string argumentos = url.Substring(7);
+            //Console.WriteLine(argumentos);
+
+
+            string texto = "Gustavo Silva";
+
+            texto.Substring(8);
+            string texto2 = texto.Substring(1);
+
+            Console.WriteLine("Sobrenome: " + texto);
+            Console.WriteLine(texto2);
+
+            Console.ReadLine();
+        }
+
+    }
+}
