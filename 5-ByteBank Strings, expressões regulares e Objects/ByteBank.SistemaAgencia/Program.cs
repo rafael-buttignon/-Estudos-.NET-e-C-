0b2aee0339f94699131982d7becad0c2a7192702
Rafael Buttignon <rafaaugustocontato@gmail.com>
2020-08-04 22:54:32 -0300

    IndexOf, Substring, Remove, etc...

--- 5-ByteBank Strings, expressões regulares e Objects/ByteBank.SistemaAgencia/Program.cs
+++ 5-ByteBank Strings, expressões regulares e Objects/ByteBank.SistemaAgencia/Program.cs
@@ -9,20 +9,62 @@ namespace ByteBank.SistemaAgencia
     {
         static void Main(string[] args)
         {
-            string url = "pagina?argumentos";
 
-            //Console.WriteLine(url);
-            //string argumentos = url.Substring(7);
-            //Console.WriteLine(argumentos);
+            string textoVazio = "";
+            string textoNullo = null;
+            string textoTop = "Qualqueeer";
+            Console.WriteLine(String.IsNullOrEmpty(textoVazio));
+            Console.WriteLine(String.IsNullOrEmpty(textoNullo));
+            Console.WriteLine(String.IsNullOrEmpty(textoTop));
+            Console.WriteLine();
+
+            string urlParametros = "http://www.bytebank.com/cambio?moedaOrigem=real&moedaDestino=dolar&valor=1500";
+            ExtratorValorDeArgumentosURL extratorDeValores = new ExtratorValorDeArgumentosURL(urlParametros);
+
+            string valor = extratorDeValores.GetValor("moedaDestino");
+            Console.WriteLine("Valor de moedaDestino: " + valor);
+
+            string valorMoedaOrigem = extratorDeValores.GetValor("moedaOrigem");
+            Console.WriteLine("Valor de moedaOrigem: " + valorMoedaOrigem);
+
+            Console.WriteLine(extratorDeValores.GetValor("valor"));
+
+            Console.ReadLine();
 
 
-            string texto = "Gustavo Silva";
 
-            texto.Substring(8);
-            string texto2 = texto.Substring(1);
 
-            Console.WriteLine("Sobrenome: " + texto);
-            Console.WriteLine(texto2);
+            string palavra = "moedaOrigem=real&moedaDestino=dolar";
+            string nomeArgumento = "moedaDestino";
+
+            int indice = palavra.IndexOf(nomeArgumento);
+            Console.WriteLine(indice);
+
+            Console.WriteLine("Tamanho da string nomeArgumento: " + nomeArgumento.Length);
+
+            Console.WriteLine(palavra);
+            Console.WriteLine(palavra.Substring(indice));
+            Console.WriteLine(palavra.Substring(indice + nomeArgumento.Length + 1));
+
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            string url = "pagina?moedaOrigem=real&moedaDestino=dolar";
+
+            int indiceInterrogacao = url.IndexOf('?');
+            Console.WriteLine(indiceInterrogacao);
+
+            Console.WriteLine(url);
+            string argumentos = url.Substring(indiceInterrogacao + 1);
+            Console.WriteLine(argumentos);
+
+
+            //string texto = "Gustavo Silva";
+            //texto.Substring(8);
+            //string texto2 = texto.Substring(1);
+            //Console.WriteLine("Sobrenome: " + texto);
+            //Console.WriteLine(texto2);
 
             Console.ReadLine();
         }
