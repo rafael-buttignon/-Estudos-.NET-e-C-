39b1637502310f455cafad1169067e4d030d2f86
Rafael Buttignon <rafaaugustocontato@gmail.com>
2020-08-11 18:54:49 -0300

    Lendo Arquivos de texto, system.io

---
+++ 8- (IO) streams/ByteBankImportacaoExportacao/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+
+namespace ByteBankImportacaoExportacao
+{
+    class Program 
+    { 
+        static void Main(string[] args) 
+        {
+            var enderecoDoArquivo = "contas.txt";
+
+            var fluxoDoArquivo = new FileStream(enderecoDoArquivo, FileMode.Open);
+
+            var buffer = new byte[1024]; // 1 Kb
+
+            var numeroDeBytesLidos = -1;
+
+            while(numeroDeBytesLidos != 0)
+            {
+                numeroDeBytesLidos = fluxoDoArquivo.Read(buffer, 0, 1024);
+                EscreverBuffer(buffer);
+            }
+
+            Console.ReadLine();
+        }
+
+        static void EscreverBuffer(byte[] buffer)
+        {
+            foreach (var meuByte in buffer)
+            {
+                Console.Write(meuByte);
+                Console.Write(" ");
+            }
+        }
+    }
+} 
+ 
