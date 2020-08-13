9033774a72b126428fc2e27737392341a4d21dae
Rafael Buttignon <rafaaugustocontato@gmail.com>
2020-08-13 18:20:14 -0300

    Files, Console.OpenStardartInput(), Etc...

--- 8- (IO) streams/ByteBankImportacaoExportacao/Program.cs
+++ 8- (IO) streams/ByteBankImportacaoExportacao/Program.cs
@@ -1,6 +1,7 @@
 ﻿using ByteBankImportacaoExportacao.Modelos;
 using System;
 using System.IO;
+using System.Runtime.CompilerServices;
 using System.Text;
 
 namespace ByteBankImportacaoExportacao
@@ -9,8 +10,38 @@ namespace ByteBankImportacaoExportacao
     { 
         static void Main(string[] args) 
         {
-            //TestaEscrita();
-            LeituraBinaria();
+
+            File.WriteAllText("escrevendoComAClasseFile.txt", "Testando File.WriteAllText");
+
+            Console.WriteLine("Arquivo escrevendoComAClasseFile criado!");
+
+            Console.ReadLine();
+
+
+            var bytesArquivo = File.ReadAllBytes("contas.txt");
+            
+            Console.WriteLine($"Arquivo contas.txt pussui {bytesArquivo.Length} ");
+
+
+           var linhas = File.ReadAllLines("contas.txt", Encoding.UTF8);
+            Console.WriteLine(linhas.Length);
+
+            foreach (var item in linhas)
+            {
+                Console.WriteLine(item);
+            }
+
+            var nome = Console.ReadLine();
+
+
+            Console.WriteLine("Digite o seu nome: ");
+            var nomex = Console.ReadLine();
+
+            Console.WriteLine(nomex);
+
+
+            UsarStreamDeEntrada();
+
             Console.WriteLine("Aplicação Finalizada...");
             Console.ReadLine();
         }
