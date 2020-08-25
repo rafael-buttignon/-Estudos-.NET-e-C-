314c9721c0a3e60277383e1baa7975cc351c2a3f
Rafael Buttignon <rafaaugustocontato@gmail.com>
2020-08-25 18:37:40 -0300

    SortedDictionary(Arvore Binaria) e SortedSet (Conjunto Ordenado)

---
+++ 11- Collections Parte 2/1-CsharpCollectionsSortedList/2-CsharpCollectionsSortedDictionary/Program.cs
@@ -0,0 +1,28 @@
+﻿using _1_CsharpCollectionsSortedList;
+using System;
+using System.Collections.Generic;
+
+namespace _2_CsharpCollectionsSortedDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IDictionary<string, Aluno> sorted = new SortedDictionary<string, Aluno>();
+
+            sorted.Add("VT", new Aluno("Vanessa", 34672));
+            sorted.Add("AL", new Aluno("Ana", 5617));
+            sorted.Add("RN", new Aluno("Rafael", 17645));
+            sorted.Add("WM", new Aluno("Wanderson", 11287));
+
+            foreach (var item in sorted)
+            {
+                Console.WriteLine(item);
+            }
+
+            //Uma SortedDictionary é mais adequada se você precisa inserir / remover elementos 
+            //    com muita frequência, pois essas operações são otimizadas pela sua estrutura 
+            //    interna de árvore binária balanceada.
+        }
+    }
+}
---
+++ 11- Collections Parte 2/1-CsharpCollectionsSortedList/3-CsharpCollectionsSortedSet/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+
+namespace _3_CsharpCollectionsSortedSet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ISet<string> alunos = new SortedSet<string>(new ComparadorMinusculo())
+            {
+                "Vanessa Tonini",
+                "Ana Losnak",
+                "Rafael Nercessian",
+                "Priscila Stuani"
+            };
+
+            alunos.Add("Rafael Rollo");
+            alunos.Add("Fabio Gushiken");
+            alunos.Add("Fabio Gushiken");
+            alunos.Add("FABIO GUSHIKEN");
+
+            foreach (var aluno in alunos)
+            {
+                Console.WriteLine(aluno);
+            }
+
+            ISet<string> outroConjunto = new HashSet<string>();
+
+            //este conjunto é subconjunto do outroConjunto - IsSubSetOf
+            alunos.IsSubsetOf(outroConjunto);
+
+            //este conjunto e superconjunto do outroConjunto - IsSuperSetOf
+            alunos.IsSupersetOf(outroConjunto);
+
+            //os conjuntos contêm os mesmo elemtnos - SetEquals
+            alunos.SetEquals(outroConjunto);
+
+            //substrai os elementos da outra coleção que também estão no conjunto
+            alunos.ExceptWith(outroConjunto);
+
+            //intersecção dos conjuntos - intersectWith
+            alunos.IntersectWith(outroConjunto);
+
+            //somente em um ou outro conjunt - SymmetricExceptWith
+            alunos.SymmetricExceptWith(outroConjunto);
+
+            //união de conjuntos - UnionWith
+            alunos.UnionWith(outroConjunto);
+        }
+    }
+
+    internal class ComparadorMinusculo : IComparer<string>
+    {
+        public int Compare([AllowNull] string x, [AllowNull] string y)
+        {
+            return string.Compare(x, y, StringComparison.InvariantCultureIgnoreCase);
+        }
+    }
+}
