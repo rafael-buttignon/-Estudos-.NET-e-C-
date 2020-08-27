9c8aab7d6c1ed1eb7f8d525f6f8fb12b26d71521
Rafael Buttignon <rafaaugustocontato@gmail.com>
2020-08-27 17:53:42 -0300

    Entendo o Laço Foreach

---
+++ 11- Collections Parte 2/1-CsharpCollectionsSortedList/10-Foreach/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+
+namespace _10_Foreach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var messes = new List<string>
+            {
+                "Janeiro", "Fevereiro", "Março",
+                "Abril", "Maio", "Junho",
+                "Julho", "Agosto", "Setembro",
+                "Outubro", "Novembro", "Dezembro"
+            };
+
+            foreach (var mes in messes)
+            {
+                Console.WriteLine(mes);
+            }
+
+            //O laço foreach pode varrer qualquer coleção que implementa a interface IEnumerable.
+        }
+    }
+}
