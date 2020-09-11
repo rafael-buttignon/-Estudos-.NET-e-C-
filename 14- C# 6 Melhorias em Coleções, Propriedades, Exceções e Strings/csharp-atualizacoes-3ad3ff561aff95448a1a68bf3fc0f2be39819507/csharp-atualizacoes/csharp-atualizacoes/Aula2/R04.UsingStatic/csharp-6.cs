d8e68b08451a3f0f59163a3829344aa2968ba672
Rafael Buttignon <rafaaugustocontato@gmail.com>
2020-09-10 22:26:25 -0300

    Strings mais intelligentes com null-condicionais

--- 14- C# 6 Melhorias em Coleções, Propriedades, Exceções e Strings/csharp-atualizacoes-3ad3ff561aff95448a1a68bf3fc0f2be39819507/csharp-atualizacoes/csharp-atualizacoes/Aula2/R04.UsingStatic/csharp-6.cs
+++ 14- C# 6 Melhorias em Coleções, Propriedades, Exceções e Strings/csharp-atualizacoes-3ad3ff561aff95448a1a68bf3fc0f2be39819507/csharp-atualizacoes/csharp-atualizacoes/Aula2/R04.UsingStatic/csharp-6.cs
@@ -15,6 +15,39 @@ namespace CSharp6.R04
         {
             WriteLine("4. Using Static");
 
+            Aluno aluno = new Aluno("Marty", "McFly", new DateTime(1999, 12, 06))
+            { Endereco = "9303 Lyon Driver Hill Valey Ca", Telefone = "444-45643"};
+
+            WriteLine(aluno.Nome);
+            WriteLine(aluno.Sobrenome);
+            WriteLine(aluno.NomeCompleto);
+
+            WriteLine("Idade: {0}", aluno.GetIdade());
+            WriteLine(aluno.DadosPessoais);
+        }
+
+        class Aluno
+        {
+            public string Nome { get; }
+            public string Sobrenome { get; }
+            public string Endereco { get; set; }
+            public string Telefone { get; set; }
+            public string DadosPessoais => Format("{0}, {1}, {2}", NomeCompleto, Endereco, Telefone);
+            public DateTime DataNascimento { get; } = new DateTime(1999, 06, 12);
+            public string NomeCompleto => Nome + " " + Sobrenome;
+            public int GetIdade() => (int)(((Now - DataNascimento).TotalDays) / 365.242199);
+
+            public Aluno(string nome, string sobrenome)
+            {
+                Nome = nome;
+                Sobrenome = sobrenome;
+            }
+            public Aluno(string nome, string sobrenome, DateTime dataNascimento)
+                : this(nome, sobrenome)
+            {
+                this.DataNascimento = dataNascimento;
+            }
+
         }
     }
 }
--- 14- C# 6 Melhorias em Coleções, Propriedades, Exceções e Strings/csharp-atualizacoes-3ad3ff561aff95448a1a68bf3fc0f2be39819507/csharp-atualizacoes/csharp-atualizacoes/Aula2/R05.OperadoresNullCondicionais/csharp-6.cs
+++ 14- C# 6 Melhorias em Coleções, Propriedades, Exceções e Strings/csharp-atualizacoes-3ad3ff561aff95448a1a68bf3fc0f2be39819507/csharp-atualizacoes/csharp-atualizacoes/Aula2/R05.OperadoresNullCondicionais/csharp-6.cs
@@ -15,6 +15,75 @@ namespace CSharp6.R05
         public void Main()
         {
             WriteLine("5. Operadores Null-Condicionais");
+
+            Aluno aluno = new Aluno("Marty", "McFly", new DateTime(1999, 12, 06))
+            { Endereco = "9303 Lyon Driver Hill Valey Ca", Telefone = "444-45643" };
+
+            WriteLine(aluno.Nome);
+            WriteLine(aluno.Sobrenome);
+            WriteLine(aluno.NomeCompleto);
+
+            WriteLine("Idade: {0}", aluno.GetIdade());
+            WriteLine(aluno.DadosPessoais);
+
+            aluno.AdicionarAvaliacao(new Avaliacao(1, "Geografia", 8.0));
+            aluno.AdicionarAvaliacao(new Avaliacao(1, "Historia", 9.0));
+            aluno.AdicionarAvaliacao(new Avaliacao(1, "Matemática", 7.0));
+
+            Aluno aluno2 = new Aluno("Bart", "Simpson");
+            ImprimirMelhorNota(aluno);
+            ImprimirMelhorNota(aluno2);
+
+        }
+
+        private static void ImprimirMelhorNota(Aluno aluno)
+        {
+            Console.WriteLine("Melhor nota: {0} ", aluno?.MelhorAvaliacao?.Nota);
+        }
+
+        class Aluno
+        {
+            public string Nome { get; }
+            public string Sobrenome { get; }
+            public string Endereco { get; set; }
+            public string Telefone { get; set; }
+            public string DadosPessoais => Format("{0}, {1}, {2}", NomeCompleto, Endereco, Telefone);
+            public DateTime DataNascimento { get; } = new DateTime(1999, 06, 12);
+            public string NomeCompleto => Nome + " " + Sobrenome;
+            public int GetIdade() => (int)(((Now - DataNascimento).TotalDays) / 365.242199);
+
+            public Aluno(string nome, string sobrenome)
+            {
+                Nome = nome;
+                Sobrenome = sobrenome;
+            }
+            public Aluno(string nome, string sobrenome, DateTime dataNascimento)
+                : this(nome, sobrenome)
+            {
+                this.DataNascimento = dataNascimento;
+            }
+            private IList<Avaliacao> avaliacoes = new List<Avaliacao>();
+            public IReadOnlyCollection<Avaliacao> Avaliacoes => new ReadOnlyCollection<Avaliacao>(avaliacoes);
+
+            public void AdicionarAvaliacao(Avaliacao avaliacao)
+            {
+                avaliacoes.Add(avaliacao);
+            }
+            public Avaliacao MelhorAvaliacao => avaliacoes.OrderBy(a => a.Nota).LastOrDefault();
+        }
+
+        class Avaliacao
+        {
+            public Avaliacao(int bimestre, string materia, double nota)
+            {
+                Bimestre = bimestre;
+                Materia = materia;
+                Nota = nota;
+            }
+
+            public int Bimestre { get; }
+            public string Materia { get; }
+            public double Nota { get; }
         }
     }
 }
