7e9d24e7c3d5509a23a77f7887c0d309c5d4b38b
Rafael Buttignon <rafaaugustocontato@gmail.com>
2020-09-10 21:47:11 -0300

    SintaxeDeExpressão em Propriedades e metodos

--- 14- C# 6 Melhorias em Coleções, Propriedades, Exceções e Strings/csharp-atualizacoes-3ad3ff561aff95448a1a68bf3fc0f2be39819507/csharp-atualizacoes/csharp-atualizacoes/Aula1/R03.MembrosComSintaxeDeExpressao/csharp-6.cs
+++ 14- C# 6 Melhorias em Coleções, Propriedades, Exceções e Strings/csharp-atualizacoes-3ad3ff561aff95448a1a68bf3fc0f2be39819507/csharp-atualizacoes/csharp-atualizacoes/Aula1/R03.MembrosComSintaxeDeExpressao/csharp-6.cs
@@ -12,6 +12,37 @@ namespace CSharp6.R03
         {
             Console.WriteLine("3. Membros Com Corpo De Expressão");
 
+
+            Aluno aluno = new Aluno("Marty", "McFly", new DateTime(1999, 12, 06));
+            Console.WriteLine(aluno.Nome);
+            Console.WriteLine(aluno.Sobrenome);
+            Console.WriteLine(aluno.NomeCompleto);
+            
+            Console.WriteLine("Idade: {0}", aluno.GetIdade());
+        }
+
+        class Aluno
+        {
+            public string Nome { get; }
+
+            public string Sobrenome { get; }
+
+            public DateTime DataNascimento { get; } = new DateTime(1999, 06, 12);
+
+            public string NomeCompleto  => Nome + " " + Sobrenome;
+            public int GetIdade() => (int)(((DateTime.Now - DataNascimento).TotalDays) / 365.242199);
+
+
+            public Aluno(string nome, string sobrenome)
+            {
+                Nome = nome;
+                Sobrenome = sobrenome;
+            }
+            public Aluno(string nome, string sobrenome, DateTime dataNascimento)
+                : this(nome, sobrenome)
+            {
+                this.DataNascimento = dataNascimento;
+            }
         }
     }
-}
+ }
