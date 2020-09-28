0cbe02898badfce1e3ef7faf0daffe0231023f29
Rafael Buttignon <rafaaugustocontato@gmail.com>
2020-09-27 21:39:30 -0300

    Comportamentos e Eventos

---
+++ 17- C# Eventos, Delegates e Lambda/csharp-eventos-delegates-lambda-Aula1/ByteBank.Agencias/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using ByteBank.Agencias.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ByteBank.Agencias
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly ByteBankEntities _contextoBancoDeDados = new ByteBankEntities();
+        private readonly AgenciasListBox lstAgencias;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            lstAgencias = new AgenciasListBox(this);
+            AtualizarControles();
+        }
+
+        private void AtualizarControles()
+        {
+            lstAgencias.Width = 270;
+            lstAgencias.Height = 290;
+
+            Canvas.SetTop(lstAgencias, 15);
+            Canvas.SetLeft(lstAgencias, 15);
+
+            container.Children.Add(lstAgencias);
+
+            lstAgencias.Items.Clear();
+            var agencias = _contextoBancoDeDados.Agencias.ToList();
+            foreach (var agencia in agencias)
+                lstAgencias.Items.Add(agencia);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var confirmacao = 
+                MessageBox.Show(
+                    "Você deseja realmente excluir este item?",
+                    "Confirmação",
+                    MessageBoxButton.YesNo);
+            if (confirmacao == MessageBoxResult.Yes)
+            {
+                //Excluir
+            }
+            else
+            {
+                //Não faz nada
+            }
+        }
+    }
+}
