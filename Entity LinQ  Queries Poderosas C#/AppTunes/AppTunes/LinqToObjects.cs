ed3c2a43b5ab72cab4e87f746572a8809af8a03b
Rafael Buttignon <rafaaugustocontato@gmail.com>
2020-10-09 18:35:23 -0300

    Linq to entities

---
+++ Entity LinQ  Queries Poderosas C#/AppTunes/AppTunes/LinqToObjects.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AppTunes
+{
+    internal class LinqToObjects
+    {
+        private static void Main(string[] args)
+        {
+            //listar os gêneros rock
+            var generos = new List<Genero>
+            {
+                new Genero {Id = 1, Nome = "Rock"},
+                new Genero {Id = 2, Nome = "Reggae"},
+                new Genero {Id = 3, Nome = "Rock progressivo"},
+                new Genero {Id = 4, Nome = "Punk Rock"},
+                new Genero {Id = 5, Nome = "Clássica"},
+            };
+
+            foreach (var genero in generos)
+            {
+                if (genero.Nome.Contains("Rock"))
+                {
+                    Console.WriteLine("{0}\t{1}", genero.Id, genero.Nome);
+                }
+            }
+
+            Console.WriteLine();
+
+            var musicas = new List<Musica>
+            {
+                new Musica { Id = 1, Nome= "Sweet Child O Mine", GeneroId = 1},
+                new Musica { Id = 2, Nome= "I Shot The Sheriff", GeneroId = 2},
+                new Musica { Id = 3, Nome= "Danúbio Azul", GeneroId = 5}
+            };
+
+            var musicaQuery = from m in musicas
+                              join g in generos on m.GeneroId equals g.Id
+                              select new { m, g};
+            
+            foreach (var musica in musicaQuery)
+            {
+                Console.WriteLine("{0}\t{1}\t{2}", musica.m.Id, musica.m.Nome, musica.g.Nome);
+            }
+
+            Console.ReadKey();
+        }
+
+        private class Genero
+        {
+            public int Id { get; set; }
+            public string Nome { get; set; }
+        }
+
+        class Musica
+        {
+            public int Id { get; set; }
+            public string Nome { get; set; }
+            public int GeneroId { get; set; }
+        }
+    }
+}
---
+++ Entity LinQ  Queries Poderosas C#/AppTunes/AppTunes/LinqToXml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using System.Xml.Linq;
+
+class LinqToXml
+{
+    static void Main(string[] args)
+    {
+        XElement root = XElement.Load(@"Data\AluraTunes.xml");
+
+        var queryXML = 
+            from g in root.Element("Generos").Elements("Genero")
+                       select g;
+
+        foreach (var genero in queryXML)
+        {
+            Console.WriteLine("{0}{1}", genero.Element("GeneroId").Value, genero.Element("Nome").Value);
+        }
+
+        var query = from g in root.Element("Generos").Elements("Genero")
+                    join m in root.Element("Musicas").Elements("Musica")
+                    on g.Element("GeneroId").Value equals m.Element("GeneroId").Value
+                    select new
+                    {
+                        Musica = m.Element("Nome").Value,
+                        Genero = g.Element("Nome").Value
+                    };
+        
+        Console.WriteLine();
+
+        foreach (var musicaGenero in query)
+        {
+            Console.WriteLine("{0}\t{1}", musicaGenero.Musica, musicaGenero.Genero);
+        }
+
+        Console.ReadKey();
+    }
+}
