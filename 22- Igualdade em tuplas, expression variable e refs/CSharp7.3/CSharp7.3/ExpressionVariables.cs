e9ac0e1fe37d8bfbe48459ada4c4526909a59d8d
Rafael Buttignon <rafaaugustocontato@gmail.com>
2020-10-20 23:49:11 -0300

    Expression Variables

---
+++ 22- Igualdade em tuplas, expression variable e refs/CSharp7.3/CSharp7.3/ExpressionVariables.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace CSharp7._3
+{
+    class AplicacaoWeb
+    {
+        public static string Porta = "8080";
+
+        public static bool EstaRodandoEmPortaAlta =
+            int.TryParse(Porta, out int portaComoint) && portaComoint > 1024;
+    }
+
+
+    public class ExpressionVariables
+    {
+
+        public bool ValidadeIdade(string idadeComoTexto) =>
+            int.TryParse(idadeComoTexto, out int idade) && idade > 18;
+
+
+        public static void TestaExpressionVariables()
+        {
+            var idadeComoTexto = "15";
+
+
+            if(int.TryParse(idadeComoTexto, out int idade) && idade > 18)
+            {
+                Console.WriteLine("Você pode entrar");
+            }
+
+        }
+    }
+}
