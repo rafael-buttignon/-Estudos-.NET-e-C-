62a35724bca353b70684db9fcf416e14caedf0b0
Rafael Buttignon <rafaaugustocontato@gmail.com>
2020-10-23 18:47:58 -0300

    BackField Attributes

---
+++ 22- Igualdade em tuplas, expression variable e refs/CSharp7.3/CSharp7.3/BackfieldAttribute.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp7._3
+{
+    [Serializable]
+    public class FormularioCadastro
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        private string _senha;
+        [field: NonSerialized]
+        public string Senha
+        {
+            get
+            {
+                return _senha;
+            }
+            set
+            {
+                _senha = value;
+            }
+        }
+    }
+
+    public class BackfieldAttribute
+    {
+        public static void Testa()
+        {
+            var novoCadastro = new FormularioCadastro
+            {
+                Nome = "Gui",
+                Email = "Gui@Alura.com.br",
+                Senha = "senha super dificil e complicada de lembrar"
+            };
+
+            using (var arquivo = File.Create("cadastro.bin"))
+            {
+                var formatador = new BinaryFormatter();
+                formatador.Serialize(arquivo, novoCadastro);
+            }
+        }
+    }
+}
